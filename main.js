d3beb4fbdda74d70a14881cfb21e515ccbde7e2b
Tiana Hayden <tiana.hayden@me.com>
2018-09-22 10:57:14 -0700

    Tiana first animation with AnimeJS

---
+++ main.js
@@ -0,0 +1,128 @@
+anime({
+    targets: 'div.box.red',
+    translateY: [
+        { value: 100, duration: 100 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    fontSize: [
+        { value: '150px' },
+        { value: '250px' }
+    ],
+    color: '#00ffe7', 
+})
+
+anime({
+    targets: 'div.box.orange',
+    translateY: [
+        { value: 100, duration: 100 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    fontSize: [
+        { value: '150px' },
+        { value: '250px' }
+    ],
+    color: '#00ead4', 
+    delay: '200'
+})
+
+anime({
+    targets: 'div.box.green',
+    translateY: [
+        { value: 100, duration: 100 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    fontSize: [
+        { value: '150px' },
+        { value: '250px' }
+    ],
+    color: '#00d5c1', 
+    delay: '400'
+})
+
+anime({
+    targets: 'div.box.blue',
+    translateY: [
+        { value: 100, duration: 100 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    fontSize: [
+        { value: '150px' },
+        { value: '250px' }
+    ],
+    color: '#00b9a8', 
+    delay: '600'
+})
+
+anime({
+    targets: 'div.box.purple',
+    translateY: [
+        { value: 100, duration: 100 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    fontSize: [
+        { value: '150px' },
+        { value: '250px' }
+    ],
+    color: '#009f91', 
+    delay: '800'
+})
+
+anime({
+    targets: 'body',
+    translateX: [
+        { value: 100, duration: 200 },
+        { value: 0, duration: 100 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: [
+        { value: '0.8' },
+        { value: '1' }
+    ],
+    backgroundColor: '#303030', 
+    delay: '1800'
+})
+
+
